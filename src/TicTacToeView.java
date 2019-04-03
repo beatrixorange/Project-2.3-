ab0885ea18551c2b2f2196631657412583fd60ce
Florian Molenaars <f.molenaars@hotmail.com>
2019-04-03 13:39:34 +0200

    add board to TicTacToeView

--- src/TicTacToeView.java
+++ src/TicTacToeView.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Cell;
@@ -8,8 +10,12 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 public class TicTacToeView extends AbstractGameView {
+	
+	private Rectangle[][] board;
+	Text text = null;
 	
 	public TicTacToeView(){
 		//Pane pane = new Pane();
@@ -22,16 +28,38 @@ public class TicTacToeView extends AbstractGameView {
 		gridPane.getColumnConstraints().add(column1);
 		for (int x = 0; x < 3; x++) {
 			for (int y = 0; y < 3; y++) {
-				Rectangle rect = new Rectangle(x*200,y*200,199,199);
-
+				Rectangle rect = new Rectangle(x,y,270,260);
+				rect.setStrokeWidth(3);
+				rect.setFill(null);
 				rect.setStroke(Color.BLACK);
-				
+				this.text = new Text();
 				gridPane.add(rect, y, x);
+				
+				board[x][y] = rect;
+				
+			}
+			
+		}
+		setOnMouseClicked(event -> {
+			if (event.getButton() == MouseButton.PRIMARY) {
+				  drawX()
 			}
 		}
 		
 		this.scene = new Scene(gridPane);
 	}
+    public String getValue() {
+        return text.getText();
+    }
+	
+	public void drawX() {
+		text.setText("X");
+	}
+	
+	public void drawY() {
+		text.setText("Y");
+	}
+
 
 	@Override
 	public String getTitle() {
