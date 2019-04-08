5a9085b15bbfbcf568a26a48a09ce8c6958996f3
Detmer Struiksma <detmer56@gmail.com>
2019-04-08 12:43:09 +0200

    Added comments

--- src/Connection/Connection.java
+++ src/Connection/Connection.java
@@ -21,6 +21,8 @@ import Connection.Events.TurnEvent;
 import Connection.Events.UpdatedPlayerListEvent;
 import Connection.Events.YourMoveEvent;
 
+// The Connection class does all the communication with the server and does communication with other classes in the project
+// When the server sends a specific command this class catches it and sends an object of the type Event containing data specific to the event.
 public class Connection extends Registrator {
 	
 	private Socket socket;
@@ -46,7 +48,7 @@ public class Connection extends Registrator {
 		loginEventTriggered = false;
 		quote = '"';
 	}
-	
+	// create new socket for connection and create new input/output stream and reader/writers
 	public void connect(String ipAdress) throws UnknownHostException, IOException {
 		socket = new Socket(ipAdress, 7789);
 		input = new InputStreamReader(socket.getInputStream());
@@ -56,7 +58,7 @@ public class Connection extends Registrator {
 		receive();
 			
 	}
-	
+	// multithreaded function that listens to the server, splits the strings and triggers events 
 	public synchronized void receive() {	
 		Thread receive = new Thread(new Runnable() {
 			public void run() {
@@ -67,38 +69,40 @@ public class Connection extends Registrator {
 						System.out.println(line);
 						if(line != null && line.startsWith("OK") || line.startsWith("ERR") || line.startsWith("SVR")) {
 							if(loggedIn == true && loginEventTriggered == false) {
-								triggerEvent(new LoginSuccesEvent());
+								triggerEvent(new LoginSuccesEvent()); // succesfully loggedin
 								loginEventTriggered = true;
 							}
 							if(line.contains("SVR PLAYERLIST")) {
-								ArrayList a = StringFormat.stringToArray(line.substring("SVR PLAYERLIST ".length()));
+								ArrayList a = StringFormat.stringToArray(line.substring("SVR PLAYERLIST ".length())); // using a string editing function to add players to an arraylist.
 								playerList = a;	
-								triggerEvent(new UpdatedPlayerListEvent());
+								triggerEvent(new UpdatedPlayerListEvent());//Send notification to all registered classes that the playerlist has been updated.
 							}
 							if(line.contains("SVR GAMELIST")) {
 								ArrayList b = StringFormat.stringToArray(line.substring("SVR GAMELIST ".length()));
 								gameList = b;
 							}
 							if(line.contains("SVR GAME CHALLENGE {CHALLENGER")) {
-								String challenger = StringFormat.stringFormat(line.substring("SVR GAME CHALLENGE {CHALLENGER: ".length()));
+								// If another player challenges us the player's username and the challengenumber will be passed to the ChallengedEvent object
+								String challenger = StringFormat.stringFormat(line.substring("SVR GAME CHALLENGE {CHALLENGER: ".length())); // same string editing function but for strings instead of arrays.
 								System.out.println(challenger);
 								String t = "SVR GAME CHALLENGE {CHALLENGER: " + challenger + ", CHALLENGENUMBER: ";
 								String challengeNum = StringFormat.stringFormat(line.substring(t.length()));
 								challengeNum = challengeNum.replace(" ", "");
-								System.out.println(challengeNum);
 								String t2 = "SVR GAME CHALLENGE {CHALLENGER: " + challenger + ", CHALLENGENUMBER: " + challengeNum + ", GAMETYPE: ";
 								String gameType = StringFormat.stringFormat(line.substring(t2.length()+3));
-								System.out.println(gameType);
 								triggerEvent(new ChallengedEvent(challenger, gameType, Integer.parseInt(challengeNum)));
 							}
 							if(line.contains("SVR GAME CHALLENGE CANCELLED")) {
+								// If a challenged gets cancelled send a notification to all registered classes with the challengenumber passed to it.
 								String challengeNumC = StringFormat.stringFormat(line.substring("SVR GAME CHALLENGE CANCELLED {CHALLENGENUMBER: ".length()));
 								triggerEvent(new ChallengeCancelledEvent(Integer.parseInt(challengeNumC)));
 							}
 							if(line.contains("SVR GAME YOURTURN")) {
+								// send a YourMoveEvent
 								triggerEvent(new YourMoveEvent());
 							}
 							if(line.contains("SVR GAME MOVE")) {
+								// If a player makes a move send a TurnEvent containing the move and the player who made the move
 								String player = StringFormat.stringFormat(line.substring("SVR GAME MOVE {PLAYER: ".length()));
 								String b = "SVR GAME MOVE {PLAYER: " + player + ", MOVE: ";
 								String move = StringFormat.stringFormat(line.substring(b.length()));
@@ -106,6 +110,7 @@ public class Connection extends Registrator {
 								triggerEvent(new TurnEvent(player, Integer.parseInt(move)));
 							}
 							if(line.contains("SVR GAME MATCH")) {
+								// When a match starts send a MatchStartEvent to all registered classes with the playerToMove, gameType and opponent in it.
 								String playerToMove = StringFormat.stringFormat(line.substring("SVR GAME MATCH {PLAYERTOMOVE: ".length()));
 								String t = "SVR GAME MATCH {PLAYERTOMOVE: " + playerToMove + ", GAMETYPE: "; 
 								String gameType = StringFormat.stringFormat(line.substring(t.length()));
@@ -119,13 +124,13 @@ public class Connection extends Registrator {
 							}
 						
 							if(line.contains("WIN") || line.contains("LOSS") || line.contains("TIE")) {
+								// Send the match finished score to all registered classes
 								subscribed = false;
 								if(line.contains("WIN")){
 									String playerOneScore = StringFormat.stringFormat(line.substring("SVR GAME WIN {PLAYERONESCORE: ".length()));
 									String t = "SVR GAME WIN {PLAYERONESCORE: " + playerOneScore + ", PLAYERTWOSCORE: ";
 									String playerTwoScore = StringFormat.stringFormat(line.substring(t.length()));
 									playerTwoScore = playerTwoScore.replace(" ", "");
-									//SVR GAME WIN {PLAYERONESCORE: "0", PLAYERTWOSCORE: "0", COMMENT: "Turn timelimit reached"}
 									triggerEvent(new MatchWonEvent(Integer.parseInt(playerOneScore), Integer.parseInt(playerTwoScore)));
 								}
 								if(line.contains("LOSS")){
@@ -133,7 +138,6 @@ public class Connection extends Registrator {
 									String t = "SVR GAME LOSS {PLAYERONESCORE: " + playerOneScore + ", PLAYERTWOSCORE: ";
 									String playerTwoScore = StringFormat.stringFormat(line.substring(t.length()));
 									playerTwoScore = playerTwoScore.replace(" ", "");
-									//SVR GAME LOSS {PLAYERONESCORE: "0", PLAYERTWOSCORE: "0", COMMENT: "Turn timelimit reached"}
 									triggerEvent(new MatchLostEvent(Integer.parseInt(playerOneScore), Integer.parseInt(playerTwoScore)));
 								}
 								if(line.contains("TIE")){
@@ -141,7 +145,6 @@ public class Connection extends Registrator {
 									String t = "SVR GAME DRAW {PLAYERONESCORE: " + playerOneScore + ", PLAYERTWOSCORE: ";
 									String playerTwoScore = StringFormat.stringFormat(line.substring(t.length()));
 									playerTwoScore = playerTwoScore.replace(" ", "");
-									//SVR GAME DRAW {PLAYERONESCORE: "0", PLAYERTWOSCORE: "0", COMMENT: "Turn timelimit reached"}
 									triggerEvent(new MatchTiedEvent(Integer.parseInt(playerOneScore), Integer.parseInt(playerTwoScore)));
 								}
 							}
@@ -164,7 +167,7 @@ public class Connection extends Registrator {
 		receive.start();
 	
 	}
-	
+	// functions sends commands to the server
     public void sendCommand(String command){
         if (writer != null) {
             try {
@@ -182,7 +185,7 @@ public class Connection extends Registrator {
         }
 
     }
-    
+    // login function
     public void login(String username) {
     	if(loggedIn == false) {
     		sendCommand("login " + username);
@@ -191,10 +194,12 @@ public class Connection extends Registrator {
     	}
     }
     
+    // getter for username
     public String getUsername() {
     	return loggedUsername;
     }
     
+    //logout function
     public void logout() {
     	if(loggedIn == true) {
     		sendCommand("logout");
@@ -208,6 +213,7 @@ public class Connection extends Registrator {
     	}
     }
     
+    //function to subscribe to a game
     public void subscribe(String gameType) {
     	if (subscribed == false) {
     		sendCommand("subscribe " + gameType);
@@ -215,37 +221,48 @@ public class Connection extends Registrator {
     	}
     }
     
+    // setter for boolean subscribed
     public void setSubscribed(boolean subscribed) {
     	this.subscribed = subscribed;
     }
     
+    //getter for subscribed
     public boolean getSubscribed() {
     	return subscribed;
     }
     
+    
+   //make move function to tell the server to make a move
     public void makeMove(int move) {
     	sendCommand("move " + move);
     }
     
+    // function to send a challenge to a specific player in a specific game
     public void sendChallenge(String player, String gameType) {
-    	char quote = '"';
     	sendCommand("challenge "  + quote + player + quote + " " + quote + gameType + quote);
     }
     
+    // function to accept a challenge
     public void acceptChallenge(int challengeNumber) {
     	sendCommand("challenge accept " + challengeNumber);
     }
     
+    //send command to server to update the game list
     public void updateGameList() {
     	sendCommand("get gamelist");  			
     }
+    
+    //returns an arraylist containing all the games
     public ArrayList getGameList() {
     	return gameList;
     }
     
+    // send command to server to update the playerlist
     public synchronized void updatePlayerList() {
         sendCommand("get playerlist");
     }
+    
+    // returns an arraylist containing all the players
     public synchronized ArrayList getPlayerList() {
     	return playerList;
     }
--- src/Connection/Registrator.java
+++ src/Connection/Registrator.java
@@ -5,16 +5,19 @@ import java.util.List;
 import Connection.Events.Event;
 import Connection.Events.EventHandler;
 
+//observer class
 public class Registrator {
 	
 	private final List <EventHandler> eventHandlers = ListModDebug.getProxy(new ArrayList());
 	
+	// classes can register to the observer here
 	public void register(EventHandler eventHandler) {
 		synchronized(eventHandlers) {
 			eventHandlers.add(eventHandler);
 		}
 	}
 	
+	// function that triggers events and takes an object of type Event which is an empty interface.
 	public void triggerEvent(Event event) {
 		synchronized(eventHandlers) {
 			for(EventHandler eventHandler : eventHandlers) {
