608262859321d27ede94bbe2358035e5a6cba3b4
remi <github@remi.im>
2019-04-09 11:37:04 +0200

    Add toString to Tile and Board

--- src/Framework/Tile.java
+++ src/Framework/Tile.java
@@ -13,4 +13,29 @@ public enum Tile
 
 		return Tile.ONE;
 	}
+
+	public static Tile other(Tile t)
+	{
+		if (t == Tile.ONE) {
+			return Tile.TWO;
+		} else if (t == Tile.TWO) {
+			return Tile.ONE;
+		}
+
+		System.out.println("waat tile.other");
+		return Tile.EMPTY;
+	}
+
+	public String toString()
+	{
+		if (this == Tile.ONE) {
+			return "1";
+		}
+		
+		if (this == Tile.TWO) {
+			return "2";
+		}
+
+		return " ";
+	}
 }
